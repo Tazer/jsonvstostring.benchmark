c2766ac49fdf9afb680f4469d83d52d949f86fbb
Patrik Potocki <rambo3swe@gmail.com>
2016-11-28 10:04:53 +0100

    just a basic test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var summary = BenchmarkRunner.Run<JsonVsString>();
+
+           
+
+            Console.Read();
+        }
+    }
+
+    public class JsonVsString
+    {
+        private readonly bool data;
+        public JsonVsString()
+        {
+            data = true;
+        }
+
+        [Benchmark]
+        public string ToString()
+        {
+            return data.ToString().ToLower();
+        }
+
+        [Benchmark]
+        public string ToJson()
+        {
+            return new JavaScriptSerializer().Serialize(data);
+        }
+
+
+        [Benchmark]
+        public string NewtonSoft()
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+    }
+}
